5631648b0f3923a7946c23e031a09e3dbfc4948c
Aaron Powell <me@aaron-powell.com>
2020-09-30 14:56:38 +1000

    implementing the token issue backend for ACS clients

--- api/issueToken/index.ts
+++ api/issueToken/index.ts
@@ -1,17 +1,46 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { CommunicationIdentityClient } from "@azure/communication-administration";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const identityClient = new CommunicationIdentityClient(
+  process.env["COMMUNICATION_SERVICES_CONNECTION_STRING"]
+);
 
+type ClientPrincipal = {
+  identityProvider: string;
+  userId: string;
+  userDetails: string;
+  userRoles: string[];
+};
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const header = req.headers["x-ms-client-principal"];
+  const encoded = Buffer.from(header, "base64");
+  const decoded = encoded.toString("ascii");
+
+  const principal: ClientPrincipal = JSON.parse(decoded);
+
+  if (!principal.userId) {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 401,
+      body: "The user name is required to ensure their access token",
     };
+    return;
+  }
 
+  const user = await identityClient.createUser();
+  const tokenResponse = await identityClient.issueToken(user, ["voip"]);
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: {
+      token: tokenResponse.token,
+      expiresOn: tokenResponse.expiresOn,
+      communicationUserId: user.communicationUserId,
+    },
+  };
 };
 
 export default httpTrigger;
