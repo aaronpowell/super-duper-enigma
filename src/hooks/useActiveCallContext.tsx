1ee41c3ccb16562a069ced26bd3451001a48cca0
Aaron Powell <me@aaron-powell.com>
2020-10-21 15:59:29 +1100

    displaying the people in the call

--- src/hooks/useActiveCallContext.tsx
+++ src/hooks/useActiveCallContext.tsx
@@ -2,8 +2,9 @@ import {
   Call,
   CallState,
   LocalVideoStream,
+  RemoteParticipant,
 } from "@azure/communication-calling";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import { useCallingContext } from "./useCallingContext";
 import { v4 as uuid } from "uuid";
 import { useHistory } from "react-router-dom";
@@ -14,6 +15,8 @@ type ActiveCallContextProps = {
   startCall: () => void;
   joinCall: (groupId: string) => void;
   call?: Call;
+  remoteParticipants: RemoteParticipant[];
+  localVideoStreams: LocalVideoStream[];
 };
 
 const ActiveCallContext = React.createContext<ActiveCallContextProps>({
@@ -21,6 +24,8 @@ const ActiveCallContext = React.createContext<ActiveCallContextProps>({
     throw new Error("Not implemented");
   },
   joinCall: nie,
+  remoteParticipants: [],
+  localVideoStreams: [],
 });
 
 export const ActiveCallContextProvider = (props: {
@@ -31,76 +36,84 @@ export const ActiveCallContextProvider = (props: {
   const { currentCamera, currentMic, name } = useUserCallSettingsContext();
   const [call, setCall] = useState<Call>();
   const [, setCallState] = useState<CallState>();
-  //   const [, setLocalVideo] = useState();
+  const [localVideoStreams, setLocalVideoStreams] = useState<
+    LocalVideoStream[]
+  >([]);
+  const [remoteParticipants, setRemoteParticipants] = useState<
+    RemoteParticipant[]
+  >([]);
+
+  const localVideoStreamsUpdated = useCallback(
+    (streams: { added: LocalVideoStream[]; removed: LocalVideoStream[] }) => {
+      setLocalVideoStreams(streams.added);
+    },
+    []
+  );
+
+  const remoteParticipantsUpdated = useCallback(
+    (streams: { added: RemoteParticipant[]; removed: RemoteParticipant[] }) => {
+      setRemoteParticipants(streams.added);
+    },
+    []
+  );
+
+  const callStateChanged = useCallback(() => {
+    if (call) {
+      setCallState(call.state);
+    }
+  }, [call]);
+
+  function connectToCall(groupId: string) {
+    if (deviceManager && callAgent) {
+      callAgent.updateDisplayName(name);
+      if (currentMic) {
+        deviceManager.setMicrophone(currentMic);
+      }
+
+      const call = callAgent.join(
+        { groupId },
+        {
+          videoOptions: {
+            localVideoStreams: currentCamera
+              ? [new LocalVideoStream(currentCamera)]
+              : [],
+          },
+        }
+      );
+
+      setCall(call);
+
+      call.on("localVideoStreamsUpdated", localVideoStreamsUpdated);
+      call.on("remoteParticipantsUpdated", remoteParticipantsUpdated);
+      call.on("callStateChanged", callStateChanged);
+
+      history.push(`/call/${groupId}`);
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      if (call) {
+        call.off("localVideoStreamsUpdated", localVideoStreamsUpdated);
+        call.off("remoteParticipantsUpdated", remoteParticipantsUpdated);
+        call.off("callStateChanged", callStateChanged);
+      }
+    };
+  }, [
+    call,
+    localVideoStreamsUpdated,
+    remoteParticipantsUpdated,
+    callStateChanged,
+  ]);
+
   return (
     <ActiveCallContext.Provider
       value={{
         call,
-        startCall: () => {
-          if (deviceManager && callAgent) {
-            callAgent.updateDisplayName(name);
-            if (currentMic) {
-              deviceManager.setMicrophone(currentMic);
-            }
-
-            const groupId = uuid();
-            const call = callAgent.join(
-              { groupId },
-              {
-                videoOptions: {
-                  localVideoStreams: currentCamera
-                    ? [new LocalVideoStream(currentCamera)]
-                    : [],
-                },
-              }
-            );
-
-            setCall(call);
-
-            call.on("localVideoStreamsUpdated", (e) => {
-              console.log(`localVideoStreamsUpdated`, e);
-            });
-            call.on("remoteParticipantsUpdated", (e) => {
-              console.log("remoteParticipantsUpdated", e);
-            });
-
-            call.on("callStateChanged", () => setCallState(call.state));
-
-            history.push(`/call/${groupId}`);
-          }
-        },
-        joinCall: (groupId) => {
-          if (deviceManager && callAgent) {
-            callAgent.updateDisplayName(name);
-            if (currentMic) {
-              deviceManager.setMicrophone(currentMic);
-            }
-
-            const call = callAgent.join(
-              { groupId },
-              {
-                videoOptions: {
-                  localVideoStreams: currentCamera
-                    ? [new LocalVideoStream(currentCamera)]
-                    : [],
-                },
-              }
-            );
-
-            setCall(call);
-
-            call.on("localVideoStreamsUpdated", (e) => {
-              console.log(`localVideoStreamsUpdated`, e);
-            });
-            call.on("remoteParticipantsUpdated", (e) => {
-              console.log("remoteParticipantsUpdated", e);
-            });
-
-            call.on("callStateChanged", () => setCallState(call.state));
-
-            history.push(`/call/${groupId}`);
-          }
-        },
+        startCall: () => connectToCall(uuid()),
+        joinCall: (groupId) => connectToCall(groupId),
+        localVideoStreams,
+        remoteParticipants,
       }}
     >
       {props.children}
--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -1,10 +1,35 @@
-import { Stack } from "@fluentui/react";
-import React from "react";
+import {
+  LocalVideoStream,
+  RemoteVideoStream,
+  Renderer,
+} from "@azure/communication-calling";
+import { Stack, StackItem } from "@fluentui/react";
+import React, { useEffect, useRef } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useActiveCallContext } from "../hooks/useActiveCallContext";
 
+const VideoDisplay = (props: {
+  stream: LocalVideoStream | RemoteVideoStream;
+}) => {
+  const vidRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const renderer = new Renderer(props.stream);
+
+    renderer.createView().then((view) => {
+      vidRef.current!.appendChild(view.target);
+    });
+  }, [props.stream, vidRef]);
+
+  return <div ref={vidRef}></div>;
+};
+
 const Call = () => {
-  const { call } = useActiveCallContext();
+  const {
+    call,
+    localVideoStreams,
+    remoteParticipants,
+  } = useActiveCallContext();
   const history = useHistory();
   const { groupId } = useParams<{ groupId: string }>();
 
@@ -22,6 +47,22 @@ const Call = () => {
         <li>Muted?: {call.isMicrophoneMuted}</li>
         <li>Identity: {call.callerIdentity}</li>
       </ul>
+      <Stack>
+        <StackItem>
+          <h2>This should be you</h2>
+          {localVideoStreams.map((stream) => (
+            <VideoDisplay stream={stream} />
+          ))}
+        </StackItem>
+        <Stack>
+          <h2>Here's your friends</h2>
+          {remoteParticipants.map((participant) =>
+            participant.videoStreams.map((stream) => (
+              <VideoDisplay stream={stream} />
+            ))
+          )}
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
